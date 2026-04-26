ce5a117d7530202f8fcca4be1b83556bbc2a5776
Sanedrac1 <91925379+Sanedrac1@users.noreply.github.com>
2026-04-26 17:09:45 -0500

    Update Git configuration for commit script

--- .github/scripts/commit-repo.sh
+++ .github/scripts/commit-repo.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 set -e
 
-git config --global user.email "156378334+keiyoushi-bot@users.noreply.github.com"
-git config --global user.name "keiyoushi-bot"
+git config --global user.email "91925379+Sanedrac1@users.noreply.github.com"
+git config --global user.name "Sanedrac-Bot"
 git status
 if [ -n "$(git status --porcelain)" ]; then
     git add .
     git commit -m "Update extensions repo"
     git push
 
-    curl https://purge.jsdelivr.net/gh/keiyoushi/extensions@repo/index.min.json
 else
     echo "No changes to commit"
 fi
